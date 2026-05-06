4f726e809c21c8dae50fc5aaac6887a17d25e502
Priyanshu jain <89932280+yash-jain2001@users.noreply.github.com>
2026-05-06 11:01:49 +0530

    checking merge

--- Dev Tinder Frontend/src/components/Connections.jsx
+++ Dev Tinder Frontend/src/components/Connections.jsx
@@ -1,4 +1,4 @@
-import axios from "axios"   .;
+import axios from "axios";
 import React, { useEffect } from "react";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
